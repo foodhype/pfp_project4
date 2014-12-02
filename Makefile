382f0f0340a2a3b23f0df45af43fc8e41baec0d5
foodhype <brianschmitz.main@gmail.com>
2014-12-02 15:16:31 -0600

    Stable.

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 run:
-	g++ -std=c++0x main.cpp -pthread -o main -I$(TACC_PAPI_INC) -L$(TACC_PAPI_LIB) -lpapi -lrt && ./main
+	g++ -std=c++0x main.cpp -pthread -o main -I$(TACC_PAPI_INC) -L$(TACC_PAPI_LIB) -lpapi && ./main > output.csv
--- main.cpp
+++ main.cpp
@@ -3,29 +3,40 @@
 #include <iostream>
 #include <mutex>
 #include <papi.h>
-#include <sched.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <time.h>
 #include <thread>
+#include <unistd.h>
 #include <vector>
 
 
 #define TOTAL_WORKLOAD 10000000
 #define MIN_THREAD_COUNT 1
-#define MAX_THREAD_COUNT 31
+#define MAX_THREAD_COUNT 32
 #define CACHE_MAX_SIZE 1000000000 // Used for clearing cache
-#define LOOP_WARMUP_ITERATIONS 1000000000 // Used for warming up program before taking measurements
+#define LOOP_WARMUP_ITERATIONS 100000000 // Used for warming up program before taking measurements
 
 
 int global_variable = 42;
 std::atomic<int> atomic_global_variable(42);
 std::mutex mtx;
-volatile std::atomic<bool> ready(false); 
+volatile bool ready = false; 
+
+
+void prepare() {
+    // Warm up the system to ensure threads run on different cores.
+    int warmup_loop_counter = 0;
+    for (warmup_loop_counter = 0;
+            warmup_loop_counter < LOOP_WARMUP_ITERATIONS;
+            ++warmup_loop_counter) {
+    }
+    // Wait until main thread sets ready to true.
+    while (!ready);
+}
+
 
 void read_global(int tid, long long workload) {
-    while (!ready);
+    prepare();
     for (long long i = 0; i < workload; ++i) {
         int temp = global_variable;
     }
@@ -33,7 +44,7 @@ void read_global(int tid, long long workload) {
 
 
 void write_global(int tid, long long workload) {
-    while (!ready);
+    prepare();
     for (long long i = 0; i < workload; ++i) {
         global_variable = tid;
     }
@@ -41,7 +52,7 @@ void write_global(int tid, long long workload) {
 
 
 void read_modify_write_global(int tid, long long workload) {
-    while (!ready);
+    prepare();
     for (long long i = 0; i < workload; ++i) {
         global_variable++;
     }
@@ -49,7 +60,7 @@ void read_modify_write_global(int tid, long long workload) {
 
 
 void atomic_read_modify_write_global(int tid, long long workload) {
-    while (!ready);
+    prepare();
     for (long long i = 0; i < workload; ++i) {
         atomic_global_variable++;
     }
@@ -57,7 +68,7 @@ void atomic_read_modify_write_global(int tid, long long workload) {
 
 
 void lock_unlock(int tid, long long workload) {
-    while (!ready);
+    prepare();
     for (long long i = 0; i < workload; ++i) {
         mtx.lock();
         mtx.unlock();
@@ -66,69 +77,70 @@ void lock_unlock(int tid, long long workload) {
 
 
 void profile(const std::function<void(int, int)> &func, std::string func_name,
-        long long workload) {
-    // Warm up the system to ensure threads run on different cores.
-  
-    int warmup_loop_counter = 0;
-    for (warmup_loop_counter = 0;
-            warmup_loop_counter < LOOP_WARMUP_ITERATIONS;
-            ++warmup_loop_counter) {
-    }
-  
-  /*
-  cpu_set_t set;
-  CPU_ZERO (&set);
-  CPU_SET(16, &set);
-  if (sched_setaffinity(getpid(), sizeof (cpu_set_t), &set))
-    {
-      std::cerr << "Error!!";
-      return;
-    }
-  */
+        long long workload, bool strong_scaling) {
     std::cout << func_name << std::endl;
     std::cout << "thread_count,runtime" << std::endl;
   
     for (int thread_count = MIN_THREAD_COUNT;
             thread_count <= MAX_THREAD_COUNT;
             ++thread_count) {
+
         // Clear all cached memory for consistent measurements.
         char cache[CACHE_MAX_SIZE];
         memset(cache, 0, sizeof cache);
         global_variable = rand();
 
-        //long_long start_time_usec = PAPI_get_real_usec();
-
         std::vector<std::thread> threads;
         for (int tid = 0; tid < thread_count; ++tid) {
-            threads.push_back(std::thread(func, tid, workload / thread_count));
+            long_long thread_workload = strong_scaling ?
+                    workload / thread_count :
+                    workload; 
+            threads.push_back(std::thread(func, tid, thread_workload));
         }
-	long_long start_time_usec = PAPI_get_real_usec();
-	ready = true;
+
+        sleep(1);
+        long_long start_time_usec = PAPI_get_real_usec();
+        ready = true;
         for (auto& t: threads) {
             t.join();
         }
-
-        long_long elapsed_time_usec = PAPI_get_real_usec() - start_time_usec;
-	ready = false;
-	/*
+        long_long end_time_usec = PAPI_get_real_usec();
+        long_long elapsed_time_usec = end_time_usec - start_time_usec;
+        ready = false;
+	    
+        /*
         std::cout << "name: " << func_name <<
                 " thread count: " << thread_count <<
                 " workload: " << workload <<
                 " time elapsed (usec): " << elapsed_time_usec << std::endl;
-	*/
-	std::cout << thread_count << "," << elapsed_time_usec << std::endl; 
+	    */
+
+	    std::cout << thread_count << "," << elapsed_time_usec << std::endl; 
     }
 
     std::cout << std::endl;
 }
 
+
+void record_measurements(bool strong_scaling) {
+    if (strong_scaling) {
+        std::cout << "\n\nSTRONG SCALING:\n\n" << std::endl;
+    } else {
+        std::cout << "\n\nWEAK SCALING:\n\n" << std::endl;
+    }
+
+    profile(read_global, "read_global", TOTAL_WORKLOAD, strong_scaling);
+    profile(write_global, "write_global", TOTAL_WORKLOAD, strong_scaling);
+    profile(read_modify_write_global, "read_modify_write_global", TOTAL_WORKLOAD, strong_scaling);
+    profile(atomic_read_modify_write_global, "atomic_read_modify_write_global", TOTAL_WORKLOAD, strong_scaling);
+    profile(lock_unlock, "lock_unlock", TOTAL_WORKLOAD, strong_scaling);
+}
+
+
 int main() {
     srand(time(NULL));
-    profile(read_global, "read_global", TOTAL_WORKLOAD);
-    profile(write_global, "write_global", TOTAL_WORKLOAD);
-    profile(read_modify_write_global, "read_modify_write_global", TOTAL_WORKLOAD);
-    profile(atomic_read_modify_write_global, "atomic_read_modify_write_global", TOTAL_WORKLOAD);
-    profile(lock_unlock, "lock_unlock", TOTAL_WORKLOAD);
+    record_measurements(true);
+    record_measurements(false);
 
     return 0;
 }
